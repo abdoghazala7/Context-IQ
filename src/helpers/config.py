12215526c1343c7f6c087617d55bea548579f93f
Abdo Ghazala <124284873+abdoghazala7@users.noreply.github.com>
2025-12-31 18:28:53 +0200

    Add LLM configuration parameters to .env.example and update Config class

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -6,8 +6,24 @@ class Config(BaseSettings):
     ALLOWED_EXTENSIONS: list[str]
     MAX_FILE_SIZE: int  # in bytes
     FILE_DEFAULT_CHUNK_SIZE: int  # in bytes
+    
     MONGODB_URL: str
     MONGODB_DATABASE: str
+
+    GENERATION_BACKEND: str
+    EMBEDDING_BACKEND: str
+
+    OPENAI_API_KEY: str = None
+    OPENAI_API_URL: str = None
+    COHERE_API_KEY: str = None
+
+    GENERATION_MODEL_ID: str = None
+    EMBEDDING_MODEL_ID: str = None
+    EMBEDDING_MODEL_SIZE: int = None
+
+    INPUT_DEFAULT_MAX_CHARACTERS: int = None
+    GENERATION_DEFAULT_MAX_TOKENS: int = None
+    GENERATION_DEFAULT_TEMPERATURE: float = None
 
     class Config:
         env_file = ".env"
