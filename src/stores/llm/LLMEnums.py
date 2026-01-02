97bd8a7079bd70142b31f5bc141d162f810d3213
Abdo Ghazala <abdoghzala20@gmail.com>
2026-01-02 04:40:35 +0200

    Refactor LLMEnums.py for improved code organization and clarity

--- src/stores/llm/LLMEnums.py
+++ src/stores/llm/LLMEnums.py
@@ -3,6 +3,7 @@ from enum import Enum
 class LLMEnums(Enum):
     OPENAI = "OPENAI"
     COHERE = "COHERE"
+    GROQ = "GROQ"
 
 class OpenAIEnums(Enum):
     SYSTEM = "system"
@@ -16,6 +17,11 @@ class CoHereEnums(Enum):
     
     DOCUMENT = "search_document"
     QUERY = "search_query"
+
+class GroqEnums(Enum):
+    SYSTEM = "system"
+    USER = "user"
+    ASSISTANT = "assistant"
 
 
 class DocumentTypeEnum(Enum):
