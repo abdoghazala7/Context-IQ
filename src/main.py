9cac44efd2bd6795540d4cbf22f1553423bf8199
Abdo Ghazala <abdoghzala20@gmail.com>
2026-01-02 04:42:48 +0200

    Refactor main.py for improved code organization and clarity

--- src/main.py
+++ src/main.py
@@ -3,32 +3,30 @@ from routes.base import Base_router
 from routes.upload import Data_router
 from motor.motor_asyncio import AsyncIOMotorClient
 from helpers.config import Config
-from providers.llm import LLMProviderFactory
+from contextlib import asynccontextmanager
+from stores.llm import LLMProviderFactory
 
-app = FastAPI()
-
-async def startup_db_client():
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Startup
     settings = Config()
     app.mongo_conn = AsyncIOMotorClient(settings.MONGODB_URL)
     app.db_client = app.mongo_conn[settings.MONGODB_DATABASE]
 
     llm_provider_factory = LLMProviderFactory(settings)
 
-     # generation client
+    #  # generation client
     app.generation_client = llm_provider_factory.create(provider=settings.GENERATION_BACKEND)
     app.generation_client.set_generation_model(model_id = settings.GENERATION_MODEL_ID)
+    app.generation_client.set_embedding_model(model_id = settings.EMBEDDING_MODEL_ID)
 
-    # embedding client
-    app.embedding_client = llm_provider_factory.create(provider=settings.EMBEDDING_BACKEND)
-    app.embedding_client.set_embedding_model(model_id=settings.EMBEDDING_MODEL_ID,
-                                             embedding_size=settings.EMBEDDING_MODEL_SIZE)
-
-async def shutdown_db_client():
+    
+    yield
+    
+    # Shutdown
     app.mongo_conn.close()
 
-
-app.router.lifespan.on_startup.append(startup_db_client)
-app.router.lifespan.on_shutdown.append(shutdown_db_client)
+app = FastAPI(lifespan=lifespan)
     
 app.include_router(Base_router)
 app.include_router(Data_router)
