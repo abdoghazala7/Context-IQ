37cf49d1b96b32fd4ce9b4c076898009af6617bb
Abdo Ghazala <abdoghzala20@gmail.com>
2026-01-05 17:02:17 +0200

    Add VectorDB client initialization in FastAPI lifespan

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from helpers.config import Config
 from contextlib import asynccontextmanager
 from stores.llm import LLMProviderFactory
+from stores.vectordb import VectorDBProviderFactory
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -14,17 +15,23 @@ async def lifespan(app: FastAPI):
     app.db_client = app.mongo_conn[settings.MONGODB_DATABASE]
 
     llm_provider_factory = LLMProviderFactory(settings)
+    vectordb_provider_factory = VectorDBProviderFactory(settings)
 
     #  # generation client
     app.generation_client = llm_provider_factory.create(provider=settings.GENERATION_BACKEND)
     app.generation_client.set_generation_model(model_id = settings.GENERATION_MODEL_ID)
     app.generation_client.set_embedding_model(model_id = settings.EMBEDDING_MODEL_ID)
 
+    # vector db client
+    app.vectordb_client = vectordb_provider_factory.create(provider=settings.VECTOR_DB_BACKEND)
+    app.vectordb_client.connect()
+
     
     yield
     
     # Shutdown
     app.mongo_conn.close()
+    app.vectordb_client.disconnect()
 
 app = FastAPI(lifespan=lifespan)
     
