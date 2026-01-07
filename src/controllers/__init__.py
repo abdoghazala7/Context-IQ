c65d93848cc0a7b1a8040c0cd626741854c85906
Abdo Ghazala <abdoghzala20@gmail.com>
2026-01-07 21:18:39 +0200

    Import NLPController in controllers module

--- src/controllers/__init__.py
+++ src/controllers/__init__.py
@@ -2,3 +2,4 @@ from .ProjectController import projectController
 from .BaseController import basecontroller
 from .UploadController import uploadcontroller
 from .ProcessController import processcontroller
+from .NLPController import NLPController
